4531a7190fe05342a371ce617dee26e2b3ef6bdd
sushideliverywang <sushidelivery.wang@gmail.com>
2025-01-04 18:50:47 -0500

    update settings.py

--- a4lamerica/settings.py
+++ a4lamerica/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
+ALLOWED_HOSTS = ['192.168.1.77', 'a4lamerica.com', '127.0.0.1', 'localhost', '192.168.1.77:8000','www.a4lamerica.com']
 
 
 # Application definition
