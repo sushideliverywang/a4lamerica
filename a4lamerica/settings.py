df6a946e4dce6e78955e8a970c7c25f472d292a6
sushideliverywang <sushidelivery.wang@gmail.com>
2025-01-04 18:43:05 -0500

    update settings.py

--- a4lamerica/settings.py
+++ a4lamerica/settings.py
@@ -25,12 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*s!!r8e)mmn8gm@otfxnziw+9m9sh7jrd(4@g^p^&wm+a&-&q3'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
 
 
 # Application definition
