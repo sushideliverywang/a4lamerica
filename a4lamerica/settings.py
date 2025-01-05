ac58338b0d60c6c7817ba0f80d3505b78ee699c8
sushideliverywang <sushidelivery.wang@gmail.com>
2025-01-04 21:31:39 -0500

    update settings.py

--- a4lamerica/settings.py
+++ a4lamerica/settings.py
@@ -147,3 +147,9 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
+
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://a4lamerica.com',
+    'http://192.168.1.77',  # 如果通过 IP 地址访问
+]
